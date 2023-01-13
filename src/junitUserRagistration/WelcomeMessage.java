cab2e1eef4a7df5ca8463503710c64ed442f2e8f
VishvaS1999 <gvishva2601@gmail.com>
2023-01-13 23:32:38 +0530

    Welcome To Junit User Registration Test Cases

---
+++ src/junitUserRagistration/WelcomeMessage.java
@@ -0,0 +1,9 @@
+package junitUserRagistration;
+
+public class WelcomeMessage
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Welcome To Junit User Registration Test Cases");
+    }
+}
